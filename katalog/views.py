5022bd7914aab1a2eda62b1e2271f765fb19860d
Rafi Madani <rafimadani@Rafis-MacBook-Pro.local>
2022-09-14 16:57:34 +0700

    membenarkan formattin nama dan student id di catalog.htlm dan views.py

--- katalog/views.py
+++ katalog/views.py
@@ -6,7 +6,8 @@ def show_catalog(request):
     data_barang_catalog = CatalogItem.objects.all()
     context = {
     'list_barang': data_barang_catalog,
-    'nama': 'Kak Rafi'
+    'nama': 'Kak Rafi',
+    'npm' : '2106750856'
     }
     return render(request, "catalog.html", context)
 
