e95256c40cf3e951c2816bc522d3253ead03f3d7
Rafi Madani <rafimadani@Rafis-MacBook-Pro.local>
2022-09-14 15:35:23 +0700

    all

--- katalog/models.py
+++ katalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class CatalogItem(models.Model):
     item_name = models.CharField(max_length=255)
     item_price = models.BigIntegerField()
-    item_stock = models.IntegerField()
     description = models.TextField()
+    item_stock = models.IntegerField()
     rating = models.IntegerField()
     item_url = models.URLField()
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+
+def show_catalog(request):
+    data_barang_catalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_barang_catalog,
+    'nama': 'Kak Rafi'
+    }
+    return render(request, "catalog.html", context)
+
+
+
+
+
+
+
