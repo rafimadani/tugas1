81091c90ac7fd132b03363f52af655aac031f094
Rafi Madani <rafimadani@Rafis-MacBook-Pro.local>
2022-09-21 13:29:28 +0700

    test drive

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class moviesItem(models.Model):
+class moviesitem(models.Model):
     watched = models.CharField(max_length=10)
     title = models.CharField(max_length=50)
     rating = models.CharField(max_length=5)
