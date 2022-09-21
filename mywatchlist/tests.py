7dc6ab687ed67a7203d12a741d15ffd8b008f02d
Rafi Madani <rafimadani@Rafis-MacBook-Pro.local>
2022-09-21 21:38:55 +0700

    final fix , test udah ga error lagi

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,22 @@
+from urllib3 import response
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+
+class UnitTest(TestCase):
+    def test_mywatchlist_html_if_exist(self):
+        response = Client().get('/mywatchlist/htmlview/')
+        self.assertEqual(response.status_code,200)
+
+    def test_mywatchlist_json__if_exist(self):
+        response = Client().get('/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
+
+    def test_mywatchlist_xml_if_exist(self):
+        response = Client().get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
+    
+    
+  
+
