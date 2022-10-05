647da28790c5f14b95af0a6d8de3fd835381fb7a
Rafi Madani <rafimadani@Rafis-MacBook-Pro.local>
2022-10-06 00:40:44 +0700

    untuk tugas 5

--- project_django/settings.py
+++ project_django/settings.py
@@ -14,6 +14,12 @@ import os
 import dj_database_url
 from pathlib import Path
 
+# Static files (CSS, JavaScript, Images)
+# httpsdocs.djangoproject.comen1.9howtostatic-files
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = 'static'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -135,9 +141,6 @@ STATICFILES_DIRS = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
-    directory.mkdir(exist_ok=True)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
